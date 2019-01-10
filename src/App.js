9123dab5776571668ec5d8dafa54a65076863a7d
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-10 09:57:37 +0700

    Fix react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router , Route , Link } from "react-router-dom";
-// import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
 import Register from "./Register";
 import TodoForm from "./TodoApp";
 import AddressBook from "./AddressBook";
+
+const NavBar = styled.nav`
+  font-family: "Dosis", sans-serif;
+  display: flex;
+  justify-content: center;
+  background: #ef6c57;
+  padding: 30px;
+  font-weight: 600;
+
+  ul {
+    text-decoration: none;
+    display: flex;
+    flex-direction: row;
+  }
+
+  li {
+    list-style: none;
+    margin: 0px 30px 0px 30px;
+  }
+
+  .linkStyle {
+    color: #ffffff;
+    text-decoration: none;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <nav>
+          <NavBar>
             <ul>
               <li>
-                <Link to="/Register">Register</Link>
+                <Link className="linkStyle" to="/Register">
+                  Register
+                </Link>
               </li>
               <li>
-                <Link to="/TodoApp">Todo App</Link>
+                <Link className="linkStyle" to="/TodoApp">
+                  Todo App
+                </Link>
               </li>
               <li>
-                <Link to="/AddressBook">Address Book</Link>
+                <Link className="linkStyle" to="/AddressBook">
+                  Address Book
+                </Link>
               </li>
             </ul>
-          </nav>
-          <Route path="/Register" exact Component={Register} />
-          <Route path="/TodoApp" Component={TodoForm} />
-          <Route path="/AddressBook" Component={AddressBook} />
-          
-          <Register />
-          <TodoForm />
-          <AddressBook />
+          </NavBar>
+          <Route path="/Register" exact component={Register} />
+          <Route path="/TodoApp" component={TodoForm} />
+          <Route path="/AddressBook" component={AddressBook} />
+
         </div>
       </Router>
     );
